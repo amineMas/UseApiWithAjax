14fdccfc09497cef31f459541ff1ab303c97f02f
Mastouri Amine <aminemastouri@gmail.com>
2019-08-02 17:22:59 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+$('#search').click(function(){
+   var name = ($('#name').val())
+    console.log('https://swapi.co/api/people/?search='+name)
+
+   $.ajax('https://swapi.co/api/people/?search='+name ,
+   {    
+       timeout: 10000,
+       dataType: 'json',
+       type: 'GET',
+       success : function(data){
+           //console.log(data.results)
+           var infos = data.results[0]
+           $('#result').html(infos.name+' ' + infos.gender)
+           
+       },
+       error : function (errorMessage) { // error callback 
+        alert(('Error: ' + errorMessage));
+    }
+   }
+
+   );
+
+
+   /*
+   console.log('https://swapi.co/api/people/?search='+name);
+
+   $.ajax({
+    url : 'https://swapi.co/api/people/?search='+name,
+    dataType: 'json',
+    Type: 'GET',
+    success: function(data){
+        console.log(data.results[0])
+        var people = data.results[0]
+        $('#result').html(people.name);
+    }
+
+   });
+
+*/
+
+   
+});
+
